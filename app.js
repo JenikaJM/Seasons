e7fe0a394dc261891da977d36004509c65ec1705
JenikaJM <67513148+JenikaJM@users.noreply.github.com>
2020-07-18 19:55:59 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+function copy() {
+    alert('LOL Avoid being lazy🥱. Try typing it!😜');
+}
+
+function summer() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005653/summer_jt8yiy.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("Yay! It is summer🌞")
+}
+
+function winter() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("brr! It is very cold🥶");
+}
+
+function autumn() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("It is Autumn🍂");
+}
+
+function spring() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("Look at the flowers grow🌸");
+}
